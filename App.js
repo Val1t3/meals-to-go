36dc121b212cc590d7c5864682217ac889f649df
Val <valentinwoehrel2003@gmail.com>
2022-10-31 08:02:30 +0100

    [ADD] eslint

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
-import { StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from "react";
+// import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 const isAndroid = Platform.OS === "android";
 
 function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.contSearch}>
-                <Text>
-                    search
-                </Text>
-            </View>
-            <View style={styles.contContent}>
-                <Text>
-                    list
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contSearch}>
+        <Text>search</Text>
+      </View>
+      <View style={styles.contContent}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: isAndroid ? StatusBar.currentHeight : 0
-    },
-    contSearch: {
-        padding: 16,
-        backgroundColor: "green",
-    },
-    contContent: {
-        flex: 1,
-        padding: 16,
-        backgroundColor: "blue",
-    }
+  container: {
+    flex: 1,
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  contSearch: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  contContent: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
 
 export default App;
