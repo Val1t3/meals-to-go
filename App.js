cbf845a6f790f7defbefd65eef5da40c4727e576
Val <valentinwoehrel2003@gmail.com>
2022-10-31 07:40:13 +0100

    [ADD] main container for IOS and Android

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const isAndroid = Platform.OS === "android";
+
+function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.contSearch}>
+                <Text>
+                    search
+                </Text>
+            </View>
+            <View style={styles.contContent}>
+                <Text>
+                    list
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: isAndroid ? StatusBar.currentHeight : 0
+    },
+    contSearch: {
+        padding: 16,
+        backgroundColor: "green",
+    },
+    contContent: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "blue",
+    }
 });
+
+export default App;
