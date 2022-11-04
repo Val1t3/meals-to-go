c4dad4747be04244eff225c24e4d1494cf8f5892
Val <valentinwoehrel2003@gmail.com>
2022-11-04 11:09:41 +0100

    [ADD] icons

--- App.js
+++ App.js
@@ -3,8 +3,25 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import RestaurantsScreen from "./src/screens/restaurants.screens";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/components/restaurant-info-card.js
+++ src/components/restaurant-info-card.js
@@ -1,6 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from "react";
 import { Card } from "react-native-paper";
+import { Text, Image, View } from "react-native";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../assets/icons/star";
+import isOpen from "../../assets/icons/isOpen";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -12,28 +17,76 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Adress = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+`;
+
+const SectionEnd = styled.View`
+  flex-direction: row;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    // icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
-    // address = "100 some random street",
-    // isOpenNow = true,
-    // rating = 4,
-    // isClosedTemporarily,
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const RatingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <>
       <RestaurantCard>
         <RestaurantCardCover source={{ uri: photos[0] }} />
-        <Title>{name}</Title>
+        <Info>
+          <Title>{name}</Title>
+          <Row>
+            <Rating>
+              {RatingArray.map(() => {
+                return <SvgXml xml={star} width={20} height={20} />;
+              })}
+            </Rating>
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <View style={{ marginLeft: 16 }} />
+              {isOpenNow && <SvgXml xml={isOpen} width={20} height={20} />}
+              <View style={{ marginLeft: 16 }} />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </SectionEnd>
+          </Row>
+          <Adress>{address}</Adress>
+        </Info>
       </RestaurantCard>
     </>
   );
